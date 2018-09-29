50fcef5d9588074cc5cfedaf3b3351af45029c7b
sandrawakabayashi <sandrawaka@gmail.com>
2018-09-29 13:14:18 -0400

    add fade and random int function

--- led.py
+++ led.py
@@ -1,13 +1,59 @@
+import sys
 import opc
 import time
 import board
 import busio
-
-from random import randint
-
 import mido
 
+from mido import Message
+from random import randint
+
 import adafruit_mpr121
+
+NUM_LEDS = 512
+
+def execute_midi():
+    """ execute midi """
+    portname = mido.get_output_names()[2]
+    notes = list(range(5, 30))
+
+    try:
+        with mido.open_output(portname, autoreset=True) as port:
+            print('Using {}'.format(port))
+            while True:
+                for note in notes:
+                    on = Message('note_on', note=note)
+                    print('Sending {}'.format(on))
+                    port.send(on)
+
+                    off = Message('note_off', note=note)
+                    print('Sending {}'.format(off))
+                    time.sleep(2.35)
+                    port.send(off)
+    except KeyboardInterrupt:
+        pass
+
+poles_led_dict = {
+    1: range(),
+    2: ,
+    3: ,
+    4: 
+}
+
+
+def fade():
+    """ fade effect """
+    client = opc.Client('localhost:7890')
+
+    black = [(0,0,0)] * NUM_LEDS
+    white = [(255,255,255)] * NUM_LEDS
+
+    while True:
+	client.put_pixels(white)
+	time.sleep(0.05) 
+
+	client.put_pixels(black)
+	time.sleep(0.05)
 
 
 def strobe():
@@ -25,6 +71,16 @@ def strobe():
 	client.put_pixels(black)
 	time.sleep(0.05)
 
+class Rand:
+    def __init__(self):
+        self.last = None
+
+    def __call__(self):
+        r = randint(0, 11)
+        while r == self.last:
+            r = random.randint(0, 11)
+        self.last = r
+        return r
 
 
 def game_mode():
@@ -39,8 +95,7 @@ def game_mode():
 
     # generate random number for touch tensor
     # mpr121 sensor has 12 sensors total, so need 0-11
-    to_be_touched = randint(0, 11)
-
+    to_be_touched = Rand()
 
     # Loop forever testing each input and printing when they're touched.
     while True:
@@ -56,11 +111,11 @@ def game_mode():
                 and current_touched & pin_bit 
                 and not last_touched & pin_bit):
                 print('{0} touched!'.format(i))
+                to_be_touched = Rand()
 
-                
-
-
-            if not current_touched & pin_bit and last_touched & pin_bit:
+            if (to_be_touched == i 
+                and not current_touched & pin_bit 
+                and last_touched & pin_bit):
                 print('{0} released!'.format(i))
 
         # Update last state and wait a short period before repeating.
