7ddfe84afdccd0ea8a0f6f4526584e3f1bf0ab8d
jamiemori <=>
2018-10-07 21:17:08 -0400

    add led queue and threading

--- led.py
+++ led.py
@@ -3,6 +3,7 @@ import opc
 import time
 import socket
 import math
+import queue
 
 import board
 import busio
@@ -13,7 +14,8 @@ from random import randint
 import adafruit_mpr121
 
 
-NUM_LEDS = 792
+# NUM_LEDS = 792
+NUM_LEDS = 512
 client = opc.Client("localhost:7890")
 k = 0.001
 
@@ -197,13 +199,26 @@ def pixel_color(t, i, n_pixels, random_values):
     return (r * 128, g * 128, b * 256)
 
 
-def miami():
+def miami(sensor_id):
     """ 
     based on:
     https://github.com/zestyping/openpixelcontrol/blob/master/python/miami.py 
     """
+    led_coordinates = {
+        0: (0, 64),
+        1: (65, 128),
+        2: (129, 192),
+        3: (193, 256), 
+        4: (257, 320), 
+        5: (321, 385), 
+        6: (386, 449), 
+        7: (450, 513)
+    }
+
     random_values = [random.random() for i in range(NUM_LEDS)]
-    coordinates = list(range(130, 190))
+    coordinates = list(
+        range(led_coorindates[sensor_id][0], led_coordinates[sensor_id][1])
+    )
     start_time = time.time()
 
     while True:
@@ -253,10 +268,30 @@ class Rand:
         self.last = r
         return r
 
+led_queue = queue.Queue()
+
+def execute_lights(led_queue):
+    """ 
+    lights up light strip based on sensor_id
+
+    uses threading for non-blocking
+    """
+    while True:
+        sensor_id = led_queue.get()
+        print('current queue size ---- ', led_queue.qsize())
+        miami(sensor_id)
+        led_queue.task_done()
+
 
 def explore_mode():
     i2c = busio.I2C(board.SCL, board.SDA)
     mpr121 = adafruit_mpr121.MPR121(i2c)
+
+    worker_threads = []
+    for i in range(20):
+        t = threading.Thread(target=execute_lights, daemon=True, args=(led_queue,))
+        worker_threads.append(t)
+        t.start()
 
     last_touched = mpr121.touched()
     while True:
@@ -269,11 +304,12 @@ def explore_mode():
             pin_bit = 1 << i
             # First check if transitioned from not touched to touched.
             if current_touched & pin_bit and not last_touched & pin_bit:
-                print('{0} touched!'.format(i))
-                send(i)
+                print("{0} touched!".format(i))
+                led_queue.put(i)
+                # send(i)
 
             if not current_touched & pin_bit and last_touched & pin_bit:
-                print('{0} released!'.format(i))
+                print("{0} released!".format(i))
 
         # Update last state and wait a short period before repeating.
         last_touched = current_touched
@@ -286,84 +322,84 @@ def explore_mode():
     # last_touched = mpr121.touched()
 
     # while True:
-        # current_touched = mpr121.touched()
-        # for i in range(12):
-            # # Each pin is represented by a bit in the touched value.
-            # # A value of 1 means the pin is being touched, and 0 means
-            # # it is not being touched.
-            # pin_bit = 1 << i
+    # current_touched = mpr121.touched()
+    # for i in range(12):
+    # # Each pin is represented by a bit in the touched value.
+    # # A value of 1 means the pin is being touched, and 0 means
+    # # it is not being touched.
+    # pin_bit = 1 << i
 
-            # if (
-                # current_touched & pin_bit
-                # and not last_touched & pin_bit
-            # ):
-                # print("{0} touched!".format(i))
-                # send(i)
+    # if (
+    # current_touched & pin_bit
+    # and not last_touched & pin_bit
+    # ):
+    # print("{0} touched!".format(i))
+    # send(i)
 
-            # if (
-                # not current_touched & pin_bit
-                # and last_touched & pin_bit
-            # ):
-                # print("{0} released!".format(i))
+    # if (
+    # not current_touched & pin_bit
+    # and last_touched & pin_bit
+    # ):
+    # print("{0} released!".format(i))
 
-            # # Update last state and wait a short period before repeating.
-            # last_touched = current_touched
-            # time.sleep(0.1)
+    # # Update last state and wait a short period before repeating.
+    # last_touched = current_touched
+    # time.sleep(0.1)
 
-            # # for debugging
-            # print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x{0:0X}".format(mpr121.touched()))
-            # filtered = [mpr121.filtered_data(i) for i in range(12)]
+    # # for debugging
+    # print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x{0:0X}".format(mpr121.touched()))
+    # filtered = [mpr121.filtered_data(i) for i in range(12)]
 
-            # print("Filt:", "\t".join(map(str, filtered)))
-            # base = [mpr121.baseline_data(i) for i in range(12)]
+    # print("Filt:", "\t".join(map(str, filtered)))
+    # base = [mpr121.baseline_data(i) for i in range(12)]
 
-            # print("Base:", "\t".join(map(str, base)))
+    # print("Base:", "\t".join(map(str, base)))
 
 
 # def game_mode():
-    # i2c = busio.I2C(board.SCL, board.SDA)
-    # mpr121 = adafruit_mpr121.MPR121(i2c)
+# i2c = busio.I2C(board.SCL, board.SDA)
+# mpr121 = adafruit_mpr121.MPR121(i2c)
 
-    # # # NOTE you can optionally change the address of the device:
-    # # mpr121 = adafruit_mpr121.MPR121(i2c, address=0x91)
+# # # NOTE you can optionally change the address of the device:
+# # mpr121 = adafruit_mpr121.MPR121(i2c, address=0x91)
 
-    # # initial touch state
-    # last_touched = mpr121.touched()
+# # initial touch state
+# last_touched = mpr121.touched()
 
-    # # generate random number for touch tensor
-    # # mpr121 sensor has 12 sensors total, so need 0-11
-    # to_be_touched = Rand()
+# # generate random number for touch tensor
+# # mpr121 sensor has 12 sensors total, so need 0-11
+# to_be_touched = Rand()
 
-    # # Loop forever testing each input and printing when they're touched.
-    # while True:
-        # current_touched = mpr121.touched()
+# # Loop forever testing each input and printing when they're touched.
+# while True:
+# current_touched = mpr121.touched()
 
-        # for i in range(12):
-            # # Each pin is represented by a bit in the touched value.
-            # # A value of 1 means the pin is being touched, and 0 means
-            # # it is not being touched.
-            # pin_bit = 1 << i
+# for i in range(12):
+# # Each pin is represented by a bit in the touched value.
+# # A value of 1 means the pin is being touched, and 0 means
+# # it is not being touched.
+# pin_bit = 1 << i
 
-            # if (
-                # to_be_touched == i
-                # and current_touched & pin_bit
-                # and not last_touched & pin_bit
-                # ):
+# if (
+# to_be_touched == i
+# and current_touched & pin_bit
+# and not last_touched & pin_bit
+# ):
 
-                # print("{0} touched!".format(i))
-                # to_be_touched = Rand()
+# print("{0} touched!".format(i))
+# to_be_touched = Rand()
 
-            # if (
-                # to_be_touched == i
-                # and not current_touched & pin_bit
-                # and last_touched & pin_bit
-                # ):
+# if (
+# to_be_touched == i
+# and not current_touched & pin_bit
+# and last_touched & pin_bit
+# ):
 
-                # print("{0} released!".format(i))
+# print("{0} released!".format(i))
 
-            # # Update last state and wait a short period before repeating.
-            # last_touched = current_touched
-            # time.sleep(0.1)
+# # Update last state and wait a short period before repeating.
+# last_touched = current_touched
+# time.sleep(0.1)
 
 
 def main():
